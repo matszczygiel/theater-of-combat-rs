06a441c229ada0e56546a2cfc4158f18cecec09c
matszczygiel <mateusz.szczygiel@student.uw.edu.pl>
2019-07-24 21:36:14 +0200

    idea for units

--- src/units/unit.rs
+++ src/units/unit.rs
@@ -5,24 +5,10 @@ trait Movable {
     fn default_moving_pts() -> i32;
     fn current_moving_pts(&self) -> i32;
     fn cost_of_entering(map::filed::Field field) -> i32;
+
 }
 
-
-pub enum Unit {
-    Mechanized {
-        mv_points: i32, 
-    },
-    Armoured_cavalary {
-        mv_points: i32,
-    },
-}
-
-impl Unit {
-    pub fn get_default_mv_points(&self) -> i32 {
-        match self {
-            Unit::Mechanized => 12,
-            Unit::Armoured_cavalary => 10,
-            _=> {panic!("Unknown unit type!");}
-        }
-    }
+struct Mechanized {
+    moving_pts: i32,
+    
 }
