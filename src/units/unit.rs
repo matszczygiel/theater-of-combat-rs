327797420cf640f767b37603f6a60105da7611a3
matszczygiel <mateusz.szczygiel@student.uw.edu.pl>
2019-07-25 19:49:18 +0200

    unit trait added.

--- src/units/unit.rs
+++ src/units/unit.rs
@@ -1,6 +1,7 @@
 extern crate sfml;
 
 use crate::map::filed::Field;
+use super::token::*;
 
 pub trait Unit {
     fn default_moving_pts() -> i32;
@@ -8,6 +9,25 @@ pub trait Unit {
     fn cost_of_entering(field: Field) -> i32;
 }
 
-struct Mechanized {
+pub struct Mechanized<'a> {
     moving_pts: i32,
+    token: Token<'a>,
+}
+
+impl<'a> Unit for Mechanized<'a> {
+    fn default_moving_pts() -> i32 {
+        12
+    }
+
+    fn current_moving_pts(&self) -> i32 {
+        self.moving_pts
+    }
+
+    fn cost_of_entering(field: Field) -> i32 {
+        match field {
+            Field::Forest => 2,
+            Field::Plain =>1,
+        }
+    }
+
 }
